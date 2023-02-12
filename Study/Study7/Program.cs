587eb9b4362f5d47ed9078145f14f61a7560b12c
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-02-12 16:37:02 +0300

    добили DI, разобрали метод AddScope

---
+++ Study/Study7/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Study7.Domain;
+using Study7.Servises;
+
+IServiceCollection services = new ServiceCollection()
+    .AddScoped<ICounter, RandomCounter>();
+using ServiceProvider serviceProvider = services.BuildServiceProvider();
+PrintCounters();
+PrintCounters();
+void PrintCounters()
+{
+    using (IServiceScope scope = serviceProvider.CreateScope())
+    {
+        var counter1 = scope.ServiceProvider.GetService<ICounter>();
+        var counter2 = scope.ServiceProvider.GetService<ICounter>();
+        Console.WriteLine($"Counter1:{counter1.Value}; Counter2:{counter2.Value}");
+    }
+}
---
+++ Study/Study7/Servises/RandomCounter.cs
@@ -0,0 +1,25 @@
+﻿using Study7.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study7.Servises
+{
+    class RandomCounter : ICounter
+    {
+        static Random rnd = new Random();
+        private int _value;
+       
+
+        public RandomCounter()
+        {
+
+             _value = rnd.Next(0, 1000000);
+
+
+        }
+        public int Value { get { return _value; } }
+    }
+}
