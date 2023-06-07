acee9336883fa08a341584479fd11c5e4f3c6dd2
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-06-07 20:22:53 +0300

    создали слой DAL в нем создали конструктор чтобы установить свзяь с БД, создали сущность

---
+++ TruePPY/TruePPY.DAL/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruePPY.Domain.Entity;
+
+namespace TruePPY.DAL
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option) 
+        { 
+            Database.EnsureCreated();
+        }
+
+        public DbSet<SystemsPPY> SystemsPPY { get; set; }
+    }
+}
