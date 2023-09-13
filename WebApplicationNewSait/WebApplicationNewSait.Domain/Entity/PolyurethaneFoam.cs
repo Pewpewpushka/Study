c1f3632431cea3bd9c573ed81c82a5a90ed233e9
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-09-13 19:34:30 +0300

    создали первый объект в слое Domain, который содержит свойства, перечисления

---
+++ WebApplicationNewSait/WebApplicationNewSait.Domain/Entity/PolyurethaneFoam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationNewSait.Domain.Entity
+{
+    internal class PolyurethaneFoam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Properties { get; set; }
+        public decimal Price { get; set; }
+
+        public TypePolyurethaneFoam TypePolyurethaneFoam { get; set; }
+
+    }
+}
