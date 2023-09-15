030b7d229df209fa5f6634fab03c122825a646a9
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-09-15 21:19:30 +0300

    добавил слой Dal с подключением и созданием БД

---
+++ WebApplicationNewSait/WebApplicationNewSait.Dal/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplicationNewSait.Domain.Entity;
+
+namespace WebApplicationNewSait.DAL
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<PolyurethaneFoam> PolyurethaneFoam { get; set; }
+    }
+}
--- WebApplicationNewSait/WebApplicationNewSait/Program.cs
+++ WebApplicationNewSait/WebApplicationNewSait/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplicationNewSait.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connection));
 
 var app = builder.Build();
 
