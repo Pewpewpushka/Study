2aced0066e78dc104b253374ff4e161761ebf976
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-09-17 09:24:50 +0300

    создал репозиторий для работы с базой данных, реализовал их методы

---
+++ WebApplicationNewSait/WebApplicationNewSait.Dal/Interfaces/IBaseRepositories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationNewSait.DAL.Interfaces
+{
+    public interface IBaseRepositories<T>
+    {
+        bool Create(T entity);
+        T Get(int id);
+        IEnumerable<T> Select();
+        bool Delete(T entity);
+    }
+}
