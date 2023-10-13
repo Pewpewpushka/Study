fbdff1187346c71da3550e8222ee69f43a4ba2c7
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-10-13 15:46:06 +0300

    создал слой DAL и сущность Foam которая будет обращаться к БД и вытягивать отуда данные и набор объектов

---
+++ PPUmarket/PPUmarket.DAL/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PPUmarket.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPUmarket.DAL
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet <Foam> Foam { get; set; }
+
+    }
+}
--- PPUmarket/PPUmarket/Program.cs
+++ PPUmarket/PPUmarket/Program.cs
@@ -1,9 +1,12 @@
+using PPUmarket.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 //builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connection));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
