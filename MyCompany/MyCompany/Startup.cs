3b38cef5120144f6be64bca596e94466587b92e1
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-11-19 11:30:04 +0300

    добавил nuget  пакеты и подключил сервисы

--- MyCompany/MyCompany/Startup.cs
+++ MyCompany/MyCompany/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,10 +21,13 @@ namespace MyCompany
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            //добавляем подержку контроллеров и представлений (mvc)
+            services.AddControllersWithViews()
+            //
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
