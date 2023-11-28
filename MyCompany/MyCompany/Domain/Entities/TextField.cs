a14571ec5fa50d3036c23ad56246b9ccc2661d56
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-11-28 19:51:46 +0300

    создали базовые объект  и от них наследовались

--- MyCompany/MyCompany/Domain/Entities/TextField.cs
+++ MyCompany/MyCompany/Domain/Entities/TextField.cs
@@ -1,6 +1,18 @@
-﻿namespace MyCompany.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+
+namespace MyCompany.Domain.Entities
 {
-    public class TextField
+    public class TextField : EntityBase
     {
+        [Required]
+        public string  CodeWord { get; set; }
+        [Display(Name = "Название страницы (заголовок)")]
+        public override string Title { get; set; } = "Информационная страница";
+
+        [Display(Name = "Содержание страницы")]
+        public override string Text { get; set; } = "Содержание страницы заполняется администратором";
+        
     }
 }
