0f23b9351ab76c2619d1cfb376b953e4308cf2ae
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-11-30 17:10:52 +0300

    создали интерфейсы и реализовали их

--- MyCompany/MyCompany/Domain/Repositories/Abstract/ITextFieldsRepository.cs
+++ MyCompany/MyCompany/Domain/Repositories/Abstract/ITextFieldsRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace MyCompany.Domain.Repositories.Abstract
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCompany.Domain.Entities;
+namespace MyCompany.Domain.Repositories.Abstract
 {
-    public class ITextFieldsRepository
+    public interface ITextFieldsRepository
     {
+        // метод выборки всех текстовых полей
+        IQueryable<TextField>GetTextFields();
+        //выбрать текстовое поле по Id
+        TextField GetTextFieldById(Guid id);
+        //выбрать техтовое поле по его кодовому слову
+        TextField GetTextFieldCodeWord(string codeWord);
+        // сохранить изменения в БД
+        void SaveTextField(TextField entity);
+        // удалить текстовое поле
+        void DeleteTextField(Guid id);
+
     }
 }
--- MyCompany/MyCompany/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
+++ MyCompany/MyCompany/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
@@ -1,6 +1,35 @@
-﻿namespace MyCompany.Domain.Repositories.EntityFramework
+﻿using MyCompany.Domain.Entities;
+using MyCompany.Domain.Repositories.Abstract;
+using System;
+using System.Linq;
+
+namespace MyCompany.Domain.Repositories.EntityFramework
 {
-    public class EFTextFieldsRepository
+    public class EFTextFieldsRepository : ITextFieldsRepository
     {
+        public void DeleteTextField(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextField GetTextFieldById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextField GetTextFieldCodeWord(string codeWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TextField> GetTextFields()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTextField(TextField entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
