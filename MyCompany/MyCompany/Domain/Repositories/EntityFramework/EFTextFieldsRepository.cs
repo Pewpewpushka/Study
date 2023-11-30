e3eacca45d8025d515988c2c5c71407d96ab0a1e
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-11-30 17:42:38 +0300

    Создали объекты для редактирования и сохранения записей в БД

--- MyCompany/MyCompany/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
+++ MyCompany/MyCompany/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
@@ -1,35 +1,56 @@
-﻿using MyCompany.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MyCompany.Domain.Entities;
 using MyCompany.Domain.Repositories.Abstract;
 using System;
 using System.Linq;
 
 namespace MyCompany.Domain.Repositories.EntityFramework
-{
+{   
     public class EFTextFieldsRepository : ITextFieldsRepository
     {
-        public void DeleteTextField(Guid id)
+        // связь наших объектов сайта с БД
+        private readonly AppDbContext context;
+        public EFTextFieldsRepository(AppDbContext context)
         {
-            throw new NotImplementedException();
+            this.context = context;
         }
-
+        // реализуем методы, обращаемся к context и выбираем все записи из таблицы TextFields
+        public IQueryable<TextField> GetTextFields()
+        {
+            return context.TextFields;
+        }
+        //пишем метод если хотим выбрать одну запись FirstOrDefault
         public TextField GetTextFieldById(Guid id)
         {
-            throw new NotImplementedException();
+            return context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+        // если выбираем по ключевому слову
+        public TextField GetTextFieldByCodeWord(string codeWord)
+        {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == codeWord);
+        }
+        // сохраняем и проверяем
+        public void SaveTextField(TextField entity)
+        { // если Id  = значению по умолчанию, то предпологается что новая запись создана и Id для нее еще нет 
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;// помечаем его ключом что это новый объект , и добавит его в БД
+            else
+                // если Id  есть у объекта (не сотвествует стандартному значению Guid), помечаем флагом что он изменен есть  в БД но изменились занчения св-в 
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();// обращаемся к context  и сохраняем изменения
+        }
+        // удаляем текстовое поле из БД
+        public void DeleteTextField(Guid id)
+        {
+            // обращаемся к свойству таблицы ввызываем метод Remove создаем фековый объект и назаначаем ему id  и по нему удаляем соотв. запись
+            context.TextFields.Remove(new TextField() { Id = id });
+            context.SaveChanges();// сохраняем в  Context
         }
 
         public TextField GetTextFieldCodeWord(string codeWord)
         {
             throw new NotImplementedException();
         }
-
-        public IQueryable<TextField> GetTextFields()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SaveTextField(TextField entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
+
