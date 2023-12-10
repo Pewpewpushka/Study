cb7df99083b402c1c0f47b0a6f4a49c946acd7d9
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-12-10 08:00:21 +0300

    создали класс для авторизации

--- MyCompany/MyCompany/Service/AdminAreaAuthorization.cs
+++ MyCompany/MyCompany/Service/AdminAreaAuthorization.cs
@@ -1,6 +1,37 @@
-﻿namespace MyCompany.Service
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace MyCompany.Service
 {
-    public class AdminAreaAuthorization
+    public class AdminAreaAuthorization : IControllerModelConvention
     {
+        private readonly string area;
+        private readonly string policy;
+
+        public AdminAreaAuthorization (string area, string policy)
+        {
+            this.area = area;
+            this.policy = policy;
+        }
+
+        public void Apply (ControllerModel controller)
+        {
+
+            //проверяем для контроллера его атрибуты если присутствует атрибут AreaAttribute то добавлеям фильтр AuthorizeFilter, отпавляем пользователя на авторитизацию
+            if (controller.Attributes.Any(a =>
+                     a is AreaAttribute && (a as AreaAttribute).RouteValue.Equals(area, StringComparison.OrdinalIgnoreCase))
+                 || controller.RouteValues.Any(r =>
+                     r.Key.Equals("area", StringComparison.OrdinalIgnoreCase) && r.Value.Equals(area, StringComparison.OrdinalIgnoreCase)))
+            {
+                controller.Filters.Add(new AuthorizeFilter(policy));
+            }
+
+
+
+        }
+
     }
 }
