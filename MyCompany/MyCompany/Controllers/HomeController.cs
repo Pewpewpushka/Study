1acfb9bf9e7d41589423d5e6c273c35936e65be8
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-12-22 21:16:07 +0300

    создал два перехода это контакты и переход в БД от администратора

--- MyCompany/MyCompany/Controllers/HomeController.cs
+++ MyCompany/MyCompany/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 
 namespace MyCompany.Controllers
 {
     public class HomeController : Controller
-    {
+    {   //добавляем доступ к БД
+        readonly private DataManager dataManager;
+        // создаем текстовое поле 
+        public HomeController (DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));// предаем текстовое поле по ключевому слову
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
