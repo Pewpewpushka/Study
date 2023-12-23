239904b1d5c23dab4b9c3146fc76d502816e02c7
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-12-23 12:02:52 +0300

    добавили вкладку наши услуги services  и добаили к нему представлению

---
+++ MyCompany/MyCompany/Controllers/ServicesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using MyCompany.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MyCompany.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager dataManager;
+        public ServicesController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index(Guid id)
+        {   if(id != default)
+            {
+                return View("Show",dataManager.ServiceItems.GetServiceItemsById(id));
+            }
+            ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PageServices");
+            return View(dataManager.ServiceItems.GetServiceItems());
+        }
+    }
+}
