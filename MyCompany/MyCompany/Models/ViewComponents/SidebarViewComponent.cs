3b866013940e7ec78d5886f29a947c4dae2fe812
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2023-12-25 20:58:37 +0300

    создал представление для sidebar

--- MyCompany/MyCompany/Models/ViewComponents/SidebarViewComponent.cs
+++ MyCompany/MyCompany/Models/ViewComponents/SidebarViewComponent.cs
@@ -1,6 +1,31 @@
-﻿namespace MyCompany.Models.ViewComponents
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCompany.Domain;
+
+namespace MyCompany.Models.ViewComponents
 {
-    public class SidebarViewComponent
+    public class SidebarViewComponent: ViewComponent
     {
-    }
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+      
+        //создаем асинхронный метод возвращаем представление Default
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult) View("Default",dataManager.ServiceItems.GetServiceItems()));
+
+        }
+
+
+
+
+    }   
+    
 }
