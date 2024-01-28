d59b513f86efdf1adb52198064cf4ad1bc926e4b
DESKTOP-I4G2CLM\Admin <ottomstitory@mail.ru>
2024-01-28 10:27:49 +0300

    загрузился шаблон, после того как добавил HomeController и представление Index

---
+++ MyCompany2/MyCompany2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany2.Controllers
+{   
+    public class HomeController : Controller
+    {
+        
+     
+        public IActionResult Index()
+        {
+            return View();// выводим список всех услуг которые есть у нас на сайте
+        }
+
+
+    }
+}
